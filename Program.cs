abfb847348158ea1eb95c5a76db4a9b2d19d7459
lexchs <alexcheremisin@mail.ru>
2024-02-13 17:01:34 +0300

    start_work

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] original = new string[4] {"Hello", "2", "world", ":-)"};
+string[] newArray = new string[original.Length];
+
+void ArrayInput(string[] original, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < original.Length; i++)
+    {
+        if (original[i].Length <= 3)
+        {
+            newArray[count] = original[i];
+            count++;
+        }
+    }
+}
+
+void PrintNew(string[] array)
+{
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write($"{newArray[i]} ");
+    }
+}
+
+ArrayInput(original, newArray);
+PrintNew(newArray);
